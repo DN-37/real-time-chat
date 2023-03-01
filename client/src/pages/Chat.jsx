7838ff1b91ef206ea5d83b86c2197c25f1d62bd4
Danila Nefedov <dn375@mail.ru>
2023-03-01 13:31:45 +0400

    created component Welcome and added styles for component Contacts

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,13 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { allUsersRoute } from "../utils/APIRoutes";
 import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
 
 function Chat() {
     const navigate = useNavigate();
+    const socket = useRef();
     const [contacts, setContacts] = useState([]);
+    const [currentChat, setCurrentChat] = useState(undefined);
     const [currentUser, setCurrentUser] = useState(undefined);
 
     useEffect(async () => {
@@ -22,6 +25,13 @@ function Chat() {
         }
     }, []);
 
+    useEffect(() => {
+        if (currentUser) {
+            socket.current = io(host);
+            socket.current.emit("add-user", currentUser._id);
+        }
+    }, [currentUser]);
+
     useEffect(async () => {
         if (currentUser) {
             if (currentUser.isAvatarImageSet) {
@@ -32,17 +42,23 @@ function Chat() {
             }
         }
     }, [currentUser]);
-
+    const handleChatChange = (chat) => {
+        setCurrentChat(chat);
+    };
     return (
         <>
             <Container>
                 <div className="container">
-                    <Contacts contacts={contacts} currentUser={currentUser} />
+                    <Contacts contacts={contacts} changeChat={handleChatChange} />
+                    {currentChat === undefined ? (
+                        <Welcome />
+                    ) : ''}
                 </div>
             </Container>
         </>
     );
 }
+
 
 const Container = styled.div`
   height: 100vh;
