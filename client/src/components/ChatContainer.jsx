716d856657880e94012322249cdf36fdfd9d7c6e
Danila Nefedov <dn375@mail.ru>
2023-04-09 14:14:07 +0400

    added styles for ChatContainer

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -95,6 +95,34 @@ const Container = styled.div`
     }
   }
   .chat-messages {
-    
+    padding: 1rem 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    overflow: auto;
+    .message {
+      display: flex;
+      align-items: center;
+      .content {
+        max-width: 40%;
+        overflow-wrap: break-word;
+        padding: 1rem;
+        font-size: 1.1rem;
+        border-radius: 1rem;
+        color: #d1d1d1;
+      }
+    }
+    .sended {
+      justify-content: flex-end;
+      .content {
+        background-color: #4f04ff21;
+      }
+    }
+    .recieved {
+      justify-content: flex-start;
+      .content {
+        background-color: #9900ff20;
+      }
+    }
   }
 `;
