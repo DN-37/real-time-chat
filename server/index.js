4133a590354977569804da30654611259d36eb1d
Danila Nefedov <dn375@mail.ru>
2023-04-11 20:39:14 +0400

    added socket for connection with send-msg

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
+const socket = require("socket.io");
 
 const app = express();
 require('dotenv').config();
@@ -27,3 +28,25 @@ app.use("/api/auth", authRoutes);
 const server = app.listen(process.env.PORT,() => {
     console.log(`Server started on PORT ${process.env.PORT}`);
 })
+
+const io = socket(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      credentials: true,
+    },
+ });
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
